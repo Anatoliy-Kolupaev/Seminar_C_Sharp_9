787aa95d617c4cdba84480356e2e1fd4f05026d1
Anatoliy K <778966@mail.ru>
2022-10-13 23:43:31 +0500

    add 1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+System.Console.WriteLine("Напишите число: ");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("задайте степень вкоторую нужно возвести: ");
+int b = int.Parse(Console.ReadLine());
+
+int Work (int a, int b)
+{
+    if (b == 0) return 1;
+    else return Work(a, b-1) * a;
+}
+int sum = Work(a,b);
+System.Console.WriteLine(sum);
